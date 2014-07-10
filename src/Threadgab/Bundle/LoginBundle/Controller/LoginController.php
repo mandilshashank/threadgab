a66f11240de1cc748cb1fba9cadf4fd73e51a707
Shashank Mandil <shashankm@yahoo-inc.com>
2014-07-09 20:59:59 -0700

    Working facebook login code. Changed the name of the DefaultController to LoginController

---
+++ src/Threadgab/Bundle/LoginBundle/Controller/LoginController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Threadgab\Bundle\LoginBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Facebook\FacebookSession;
+use Facebook\FacebookRedirectLoginHelper;
+use Symfony\Component\HttpFoundation\Response;
+//use Facebook;
+
+ini_set('display_errors',"1");
+
+class LoginController extends Controller
+{
+
+	const REDIRECT_URL = "http://shashank.local/app_dev.php/redirect";
+	const appId = "709774645726735";
+	const appSecret = "9a2e31247640bca7eec80185703427ac";
+
+	function __construct() {
+		//Initialize the Facebook Session
+    	FacebookSession::setDefaultApplication(self::appId, self::appSecret);
+   	}
+
+    public function indexAction()
+    {
+    	//Redirect to Facebook login URL for logging in
+    	$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
+		$loginUrl = $helper->getLoginUrl(array( 'email', 'user_friends' ));
+
+		return new Response("<a href='".$loginUrl."'>Login using Facebook</a>");
+    }
+
+	public function getsessionAction()
+    {
+    	//Catch the redirected url and find out the session information
+    	$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
+    	
+		try {
+		  	$session = $helper->getSessionFromRedirect();
+		} catch(FacebookRequestException $ex) {
+		 	// When Facebook returns an error
+			return new Response('Got FacebookRequestException');
+		} catch(Exception $ex) {
+		  	// When validation fails or other local issues
+			return new Response('Got Some Exception');
+		}
+		if ($session) {
+		  	// Logged in
+			return new Response('Awesome !! Session token is  '.$session->getToken());
+		}
+
+		return new Response('Session is not found');
+    }    
+}
