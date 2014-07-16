955161281df650be21b90673367d8f0dc351b47b
Shashank Mandil <shashankm@yahoo-inc.com>
2014-07-16 00:43:07 -0700

    Unblocked from not being able to pass the Facebook session variables. Passing now using token and recreating wherever necessary. Token is passed using PHP session variables.

--- src/Threadgab/Bundle/LoginBundle/Controller/LoginController.php
+++ src/Threadgab/Bundle/LoginBundle/Controller/LoginController.php
@@ -4,28 +4,24 @@ namespace Threadgab\Bundle\LoginBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
 use Facebook\FacebookRedirectLoginHelper;
 use Symfony\Component\HttpFoundation\Response;
+use Threadgab\Bundle\LoginBundle\ThreadgabLoginBundle;
 
+if(!isset($_SESSION)) 
+{ 
+	session_start(); 
+} 
 ini_set('display_errors',"1");
 
 class LoginController extends Controller
 {
-
-	const REDIRECT_URL = "http://shashank.local/app_dev.php/redirect";
-	const appId = "709774645726735";
-	const appSecret = "9a2e31247640bca7eec80185703427ac";
-
-	function __construct() {
-		//Initialize the Facebook Session
-    	FacebookSession::setDefaultApplication(self::appId, self::appSecret);
-   	}
-
     public function indexAction()
     {
     	//Redirect to Facebook login URL for logging in
-    	$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
-		$loginUrl = $helper->getLoginUrl(array( 'email', 'user_friends', 'public_profile' ));
+    	$loginUrl = ThreadgabLoginBundle::getLoginUrl();
 
 		$content = $this->renderView(
 		    'ThreadgabLoginBundle:Login:index.html.twig',
@@ -33,17 +29,40 @@ class LoginController extends Controller
 		);
 
 		return new Response($content);
-
-		//return new Response("<a href='".$loginUrl."'>Login using Facebook</a>");
     }
 
 	public function getsessionAction()
     {
     	//Catch the redirected url and find out the session information
-    	$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
-    	
 		try {
-		  	$session = $helper->getSessionFromRedirect();
+
+			//Get the session id for the user
+		  	$session = ThreadgabLoginBundle::getSession();
+		  	$_SESSION['fb_token'] = $session->getToken();
+		  	//If we dont get an exception here it means that the sessoin has been active and we are
+		  	//now ready to redirect the user either to the user information page or to the main 
+		  	//discussion forum.
+
+		  	//We will be deciding to transfer to a forum or the user information page by making a 
+		  	//database query for the user to see if he/she already exists in the database.
+		  	//If they already exist to transfer them to the forum page otherwise we transfer them to the
+		  	//user information page.
+		  	if($session) {
+
+				try {
+					//Redirect to a link here with the session variable using generateUrl
+
+					$url = $this->generateUrl('threadgab_userinfo_redirect');
+					return $this->redirect($url);			
+				} catch(FacebookRequestException $e) {
+
+					//Create an error page for this scenario	
+
+					//echo "Exception occured, code: " . $e->getCode();
+					//echo " with message: " . $e->getMessage();
+				}   
+			}
+
 		} catch(FacebookRequestException $ex) {
 		 	// When Facebook returns an error
 			return new Response('Got FacebookRequestException');
@@ -51,11 +70,17 @@ class LoginController extends Controller
 		  	// When validation fails or other local issues
 			return new Response('Got Some Exception');
 		}
-		if ($session) {
-		  	// Logged in
-			return new Response('Awesome !! Session token is  '.$session->getToken());
-		}
 
 		return new Response('Session is not found');
-    }    
+    }  
+
+    public function showformAction()
+    {
+    	$session = ThreadgabLoginBundle::getSessionFromToken($_SESSION['fb_token']);
+    	if($session) {
+    		return new Response($session->getToken());
+    	} else {
+    		return new Response("Not available");
+    	}
+    }
 }
--- src/Threadgab/Bundle/LoginBundle/ThreadgabLoginBundle.php
+++ src/Threadgab/Bundle/LoginBundle/ThreadgabLoginBundle.php
@@ -3,7 +3,40 @@
 namespace Threadgab\Bundle\LoginBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Facebook\FacebookSession;
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRedirectLoginHelper;
 
 class ThreadgabLoginBundle extends Bundle
 {
+	const REDIRECT_URL = "http://shashank.local/app_dev.php/redirect";
+	const appId = "709774645726735";
+	const appSecret = "9a2e31247640bca7eec80185703427ac";
+
+	function __construct() {
+		//Initialize the Facebook Session
+    	FacebookSession::setDefaultApplication(self::appId, self::appSecret);
+   	}
+
+   	public static function getLoginUrl() {
+   		//Redirect to Facebook login URL for logging in
+    	$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
+		$loginUrl = $helper->getLoginUrl(array( 'email', 'user_friends', 'public_profile' ));
+
+		return $loginUrl;
+   	}
+
+   	public static function getSession() {
+   		$helper = new FacebookRedirectLoginHelper(self::REDIRECT_URL);
+
+		//Get the session id for the user
+	  	$session = $helper->getSessionFromRedirect();
+
+	  	return $session;
+   	}
+
+   	public static function getSessionFromToken($token) {
+   		return new FacebookSession($token);
+   	}
 }
